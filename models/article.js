4c360c2b3ee4f8a7a7b461583f045f45823a47b8
Alevtina <fedinaalevtina@gmail.com>
2020-06-25 18:57:36 +0300

    schema for article and user

--- models/article.js
+++ models/article.js
@@ -1,21 +1,28 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  name: {
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  title: {
+    type: String,
+    required: true,
+    minlength: 2,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
-  about: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  avatar: {
+  source: {
     type: String,
     required: true,
     validate: {
@@ -25,18 +32,27 @@ const userSchema = new mongoose.Schema({
       message: 'Неверный формат URL',
     },
   },
-  password: {
+  link: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(valid) {
+        return validator.isURL(valid);
+      },
+      message: 'Неверный формат URL',
+    },
+  },
+  owner: {
     type: String,
     required: true,
     select: false,
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate: {
-      validator: (v) => isEmail(v),
-      message: 'Неправильный формат почты',
-    },
-  },
 });
+
+module.exports = mongoose.model('article', articleSchema);
---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+// const validator = require('validator');
+const isEmail = require('validator/lib/isEmail');
+// const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
