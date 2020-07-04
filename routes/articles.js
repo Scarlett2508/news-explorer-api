d9958267d23e31829c6a9c24072cdfa1d5de4b3c
Alevtina <fedinaalevtina@gmail.com>
2020-07-04 22:41:21 +0300

    fixed validation

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,34 @@
 const articleRoutes = require('express').Router();
-// const auth = require('../middlewares/auth');
+const { Joi, celebrate } = require('celebrate');
+const validator = require('validator');
+
 const { getAllArticles, postArticle, deleteArticle } = require('../controllers/articles');
 
+const validUrl = (link) => {
+  if (!validator.isURL(link)) {
+    throw new Error('Неправильный формат ссылки');
+  }
+  return link;
+};
+
 articleRoutes.get('/', getAllArticles);
-articleRoutes.post('/', postArticle);
-articleRoutes.delete('/:articleId', deleteArticle);
+
+articleRoutes.post('/', celebrate({
+  body: Joi.object().keys({
+    keyword: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    date: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required().custom(validUrl),
+    image: Joi.string().required().custom(validUrl),
+  }),
+}), postArticle);
+
+articleRoutes.delete('/:articleId', celebrate({
+  body: Joi.object().keys({
+    articleId: Joi.string().alphanum().length(24),
+  }),
+}), deleteArticle);
 
 module.exports = articleRoutes;
